d3c4117568ea9ac541187710e8696fc9ae21f49b
NigarAdigozelova <72kwbg1@code.edu.az>
2023-01-13 13:35:03 +0400

    sdfsd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card';
 import Header from './components/Header';
 import Input from './components/Input';
 
 function App() {
   const [search, setSearch] = useState("")
-  const [APIData, setAPIData] = useState([])
+  const [data, setData] = useState(null)
   const [filteredResults, setFilteredResults] = useState([]);
-  const URL = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyDsE-CE_fXoriHXGL1JWzqqu9aNH-gEKNM`
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=how&key=AIzaSyDsE-CE_fXoriHXGL1JWzqqu9aNH-gEKNM`
   useEffect(() => {
     axios.get(URL)
       .then((response) => {
-        setAPIData(response.data);
+        setData(response.data.items);
       })
   }, [URL])
   
@@ -24,15 +25,10 @@ function App() {
         
         <div itemsPerRow={3} style={{ marginTop: 20 }}>
           {search.length > 1 ? (
-            filteredResults.map((item) => {
+            data.map((item,index) => {
               return (
-                <div>
-                  <div>
-                    <div>{item.name}</div>
-                    <div>
-                      {item.email}
-                    </div>
-                  </div>
+                <div key={index}>
+                 <Card a={item}/>
                 </div>
               )
             })
